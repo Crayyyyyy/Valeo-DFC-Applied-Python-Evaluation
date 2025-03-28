259fa430656a4a78343a1b505ca8db7f6d6f1ac4
RobertPlavka <robert.plavka@gmail.com>
2025-03-28 23:07:08 +0100

    Working sensor.ipynb

---
+++ values.py
@@ -0,0 +1,10 @@
+from abc import ABC, abstractmethod
+
+class Value(ABC):
+    def __init__(self, initial_value, metric):
+        self.value = initial_value 
+        self.metric = metric
+
+    @abstractmethod
+    def increment(self):
+        pass
